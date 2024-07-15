b880f53c2ba4d5588738ec71de5bdd094b8f99f1
Wizi8914 <loick.konieczny@gmail.com>
2024-07-16 01:53:38 +0200

    finish backend of contact form

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,7 @@
 const form = document.querySelector('form');
-const postUrl = "https://script.google.com/macros/s/AKfycbx1Yo2Ziv2Lu50bMJ48AQEtmp9Q63s595m3G89_kxAiOpqW5LUewClr_35qqynBFEoTmA/exec";
+const serviceID = "service_2r8tr7f";
+const templateID = "template_fslpaxf";
+
 
 function validateEmail(email) {
     var emailRegex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
@@ -10,22 +12,36 @@ function validateEmail(email) {
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const formData = new FormData(form);
-    const data = Object.fromEntries(formData);
-
-
-    fetch(postUrl, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(response => response.json())
-      .then(result => {
-        alert(result.message);
-      })
-      .catch(error => {
-        console.error('Erreur:', error);
-      });
+    sendEmail();
 });
+
+
+function sendEmail() {
+    var firstName = document.getElementById('fname').value;
+    var lastName = document.getElementById('lname').value;
+    var email = document.getElementById('email').value;
+    var object = document.getElementById('object').value;
+    var message = document.getElementById('message').value;
+
+    if (!validateEmail(email)) {
+        alert('Veuillez entrer une adresse email valide');
+        return;
+    }
+
+    var params = {
+        fname: firstName,
+        lname: lastName,
+        email: email,
+        object: object,
+        message: message
+    };
+
+    console.log(params);
+
+    emailjs.send(serviceID, templateID, params).then(function(response) {
+        alert(`Votre message a bien été envoyé`);
+        form.reset();
+    }, function(error) {
+        alert('Erreur lors de l\'envoi du message');
+    });
+};
